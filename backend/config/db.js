dace4b2910bf96347942fab6372eba808a5fc135
Moundir <sekhsoukhmoundir2007@gmail.com>
2026-04-19 19:08:23 +0100

    Switch to mongodb atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.43ajopz.mongodb.net/NeonHat?retryWrites=true&w=majority&appName=Cluster0`;
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(MONGODB_URI);
     console.log('MongoDB connecte !');
   } catch (error) {
     console.error('Erreur de connexion:', error.message);
